9d6369f4ed72c4dbc994fc4e956f55a777e78649
archigup <archigup@users.noreply.github.com>
2026-02-04 22:34:17 +0000

    Doxygen for 8638e9eee2770e4ba3bbc4180144230c1e49a7a4

--- main/navtreeindex0.js
+++ main/navtreeindex0.js
@@ -18,8 +18,8 @@ var NAVTREEINDEX0 =
 "modules.html":[3],
 "pages.html":[],
 "struct_backoff_algorithm_context__t.html":[3,0,0],
+"struct_backoff_algorithm_context__t.html#a65a8a97c7e76c727948d31fa8e2222f7":[3,0,0,2],
 "struct_backoff_algorithm_context__t.html#a842d21d7cb802443c0cc18dfe12490f9":[3,0,0,3],
-"struct_backoff_algorithm_context__t.html#aa8a4f1532288640dbdc45719bd758e21":[3,0,0,0],
-"struct_backoff_algorithm_context__t.html#ac84bf83b50d395dae1e389e0fccf1094":[3,0,0,2],
+"struct_backoff_algorithm_context__t.html#aec96ba89a84e887ed430a5089ed882fc":[3,0,0,0],
 "struct_backoff_algorithm_context__t.html#af205fc8af2271d4e709499c22d2f9b3f":[3,0,0,1]
 };
--- main/search/variables_2.js
+++ main/search/variables_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['nextjittermax_0',['nextJitterMax',['../struct_backoff_algorithm_context__t.html#ac84bf83b50d395dae1e389e0fccf1094',1,'BackoffAlgorithmContext_t']]]
+  ['nextjittermax_0',['nextJitterMax',['../struct_backoff_algorithm_context__t.html#a65a8a97c7e76c727948d31fa8e2222f7',1,'BackoffAlgorithmContext_t']]]
 ];
